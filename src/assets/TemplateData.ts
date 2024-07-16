d9326fd7295d5f683892bca0e598b1157190e62c
omkar-103 <142158418+omkar-103@users.noreply.github.com>
2024-07-16 10:34:39 +0530

    SpinWheel

---
+++ src/assets/TemplateData.ts
@@ -0,0 +1,108 @@
+export const templateItems = [
+  {
+    group: "Who's the next Participant?",
+    label: 'Omkar Parelkar',
+    weight: 5,
+    order: 0
+  },
+  {
+    group: "Who's the next Participant?",
+    label: 'Rahul',
+    weight: 1,
+    order: 1
+  },
+  {
+    group: "Who's the next Participant?",
+    label: 'Chinmay Patil',
+    weight: 1,
+    order: 2
+  },
+  {
+    group: "Who's the next Participant?",
+    label: 'Akshay Pathak',
+    weight: 1,
+    order: 3
+  },
+  {
+    group: "Who's the next Participant?",
+    label: 'Aayesha Ali',
+    weight: 4,
+    order: 4
+  },
+  {
+    group: "Who's the next Participant?",
+    label: 'Sarah Panthi',
+    weight: 1,
+    order: 5
+  },
+  {
+    group: "Who's the next Participant?",
+    label: 'Gauri Rane',
+    weight: 1,
+    order: 6
+  },
+  {
+    group: "Who's the next Participant?",
+    label: 'Mahira',
+    weight: 1,
+    order: 7
+  },
+  
+
+  {
+    group: 'Prizes',
+    label: 'iPad',
+    weight: 1,
+    order: 9
+  },
+  {
+    group: 'Prizes',
+    label: 'iPhone',
+    weight: 1,
+    order: 10
+  },
+  {
+    group: 'Prizes',
+    label: 'iPod',
+    weight: 1,
+    order: 11
+  },
+  {
+    group: 'Prizes',
+    label: 'iMac',
+    weight: 1,
+    order: 12
+  },
+  {
+    group: 'Prizes',
+    label: 'MacBook',
+    weight: 1,
+    order: 13
+  },
+  {
+    group: 'Prizes',
+    label: 'MacBook Pro',
+    weight: 1,
+    order: 14
+  },
+  {
+    group: 'Prizes',
+    label: 'MacBook Air',
+    weight: 1,
+    order: 15
+  },
+  {
+    group: 'Prizes',
+    label: 'Apple Watch',
+    weight: 1,
+    order: 16
+  },
+  {
+    group: 'Prizes',
+    label: 'Apple TV',
+    weight: 1,
+    order: 17
+  },
+
+  
+];
