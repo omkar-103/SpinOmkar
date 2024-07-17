ddf20522cb9f6de1fbff32d3894a33e14f7a1a33
omkar-103 <142158418+omkar-103@users.noreply.github.com>
2024-07-17 04:38:22 +0000

    Removed Prizes

--- src/assets/TemplateData.ts
+++ src/assets/TemplateData.ts
@@ -49,60 +49,60 @@ export const templateItems = [
   },
   
 
-  {
-    group: 'Prizes',
-    label: 'iPad',
-    weight: 1,
-    order: 9
-  },
-  {
-    group: 'Prizes',
-    label: 'iPhone',
-    weight: 1,
-    order: 10
-  },
-  {
-    group: 'Prizes',
-    label: 'iPod',
-    weight: 1,
-    order: 11
-  },
-  {
-    group: 'Prizes',
-    label: 'iMac',
-    weight: 1,
-    order: 12
-  },
-  {
-    group: 'Prizes',
-    label: 'MacBook',
-    weight: 1,
-    order: 13
-  },
-  {
-    group: 'Prizes',
-    label: 'MacBook Pro',
-    weight: 1,
-    order: 14
-  },
-  {
-    group: 'Prizes',
-    label: 'MacBook Air',
-    weight: 1,
-    order: 15
-  },
-  {
-    group: 'Prizes',
-    label: 'Apple Watch',
-    weight: 1,
-    order: 16
-  },
-  {
-    group: 'Prizes',
-    label: 'Apple TV',
-    weight: 1,
-    order: 17
-  },
+  // {
+  //   group: 'Prizes',
+  //   label: 'iPad',
+  //   weight: 1,
+  //   order: 9
+  // },
+  // {
+  //   group: 'Prizes',
+  //   label: 'iPhone',
+  //   weight: 1,
+  //   order: 10
+  // },
+  // {
+  //   group: 'Prizes',
+  //   label: 'iPod',
+  //   weight: 1,
+  //   order: 11
+  // },
+  // {
+  //   group: 'Prizes',
+  //   label: 'iMac',
+  //   weight: 1,
+  //   order: 12
+  // },
+  // {
+  //   group: 'Prizes',
+  //   label: 'MacBook',
+  //   weight: 1,
+  //   order: 13
+  // },
+  // {
+  //   group: 'Prizes',
+  //   label: 'MacBook Pro',
+  //   weight: 1,
+  //   order: 14
+  // },
+  // {
+  //   group: 'Prizes',
+  //   label: 'MacBook Air',
+  //   weight: 1,
+  //   order: 15
+  // },
+  // {
+  //   group: 'Prizes',
+  //   label: 'Apple Watch',
+  //   weight: 1,
+  //   order: 16
+  // },
+  // {
+  //   group: 'Prizes',
+  //   label: 'Apple TV',
+  //   weight: 1,
+  //   order: 17
+  // },
 
   
 ];
